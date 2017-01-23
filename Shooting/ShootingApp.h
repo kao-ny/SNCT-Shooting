9b14c9562d5a8fc49e7c5288edd83bc1c3dcddb8
h27i31 <h27i31@MICKEY42>
2017-01-23 15:04:01 +0900

    Revert "Revert "Revert "12. クラスMissile の作成"""
    This reverts commit b78f102bd6ae7c8de712d42c45724e25f8ff11b3.

--- Shooting/ShootingApp.h
+++ Shooting/ShootingApp.h
@@ -2,16 +2,13 @@
 #include "App.h"
 #include "Fighter.h"
 #include "EnemyA.h"
-#include "Missile.h"
 
 #define N_ENEMY_A 2
-#define N_MISSILE 10
 
 class ShootingApp : public App {
 private:
 	Fighter fighter;
 	EnemyA enemyA[N_ENEMY_A];
-	Missile missile[N_MISSILE];
 public:
 	ShootingApp();
 	~ShootingApp();
